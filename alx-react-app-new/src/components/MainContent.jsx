bc8f55340694f51a4eab9c87837861fabf26ed36
Christine <wanguichristine852@gmail.com>
2025-09-04 17:20:15 +0300

    Added inline CSS styling to components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', textAlign: 'center', backgroundColor: '#eef2f3' }}>
+      <h2 style={{ color: 'darkgreen' }}>Welcome to My Page</h2>
+      <p style={{ fontSize: '16px' }}>Here you’ll find information about my favorite cities around the world.</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
