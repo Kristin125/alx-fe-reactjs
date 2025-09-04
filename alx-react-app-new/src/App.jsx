b234a8af0e154763ad94ca225bc5f757ce4a1455
Christine <wanguichristine852@gmail.com>
2025-09-04 17:40:17 +0300

    Force commit App.jsx changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,28 +7,31 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Header />
-      <MainContent />
-      <Footer />
-    <div style={{ padding: 24 }}>
-      <WelcomeMessage />
-    </div>
-    <div>
+      <div>
+        <h1 style={{ textAlign: 'center' }}>React Counter App</h1>
+        <Counter />
+      </div>
       <Header />
       <MainContent />
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
       <Footer />
-    </div>
+      <div style={{ padding: 24 }}>
+        <WelcomeMessage />
+      </div>
+      <div>
+        <Header />
+        <MainContent />
+        <UserProfile 
+          name="Alice" 
+          age="25" 
+          bio="Loves hiking and photography" 
+        />
+        <Footer />
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -39,8 +42,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          count is placeholder
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
