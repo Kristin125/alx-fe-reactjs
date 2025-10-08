b2d37c8ad3f49fab5f2fa83fb7e6563e6ce5a997
Christine <wanguichristine852@gmail.com>
2025-10-08 21:55:52 +0300

    Fix routing paths and move Profile component to src/components

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import Profile from "./components/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./components/ProfileSettings";
 import Post from "./pages/Post";
